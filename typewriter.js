eb4900f34c72d117a88f201a25bddc13ca5f31f7
Rachel PR <rplourder@gmail.com>
2022-02-17 08:34:23 -0400

    A function that will take a sentence and print it out letter by letter

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+//A function that will take a sentence and print it out letter by letter
+const typeWriter = function (sentence) {
+  //sentence = "Hello there from Lower Sackville" + "\n"
+
+  for (let i = 0; i < sentence.length; i++) {
+    setTimeout(() => {
+      process.stdout.write(sentence[i])
+    }, 50 * (i + 50))
+  }
+}
+
+typeWriter("Hello there from Lower Sackville" + "\n")
